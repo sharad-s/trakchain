d3f1862a11f4168ca70690e87e14cf2fb6ed1235
sharad-s <sharadshekar@gmail.com>
2018-05-04 21:24:19 -0500

    Dumb images

--- src/components/pages/audiopage.js
+++ src/components/pages/audiopage.js
@@ -10,105 +10,105 @@ const audioJSON = [
     "name": "LILITH",
     "artist": "Joseph L'etranger",
     "fileHash": "QmbiHD9x9zrbMx7Z5yjfyjxfYJKTFMdGiPAeHBr5jYUmxE",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUBTAg3oVd68Yrrn887UtVwkbqbrbWCeuMVNTPT4zZQ1e",
     "fileID": 1,
   },
   {
     "name": "Uzi",
     "artist": "Octbr",
     "fileHash": "QmWL4AKE52rWc2YxfVVyE5MNNDWBRSKkqqZCPZWsK6PmJf",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmYVaLnSdVTDEvVPEPmHabyCVV81WP9BPG7BvrMnv8xB9m",
     "fileID": 2
   },
   {
     "name": "ZZZ",
     "artist": "Joseph L'etranger",
     "fileHash": "Qmdw3m5ZdoDT4Z8JHE6JMPwz8gtbVyvUY1B2wMBCxPgCv6",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUBTAg3oVd68Yrrn887UtVwkbqbrbWCeuMVNTPT4zZQ1e",
     "fileID": 3
   },
   {
     "name": "You Was Right",
     "artist": "Eric Dingus",
     "fileHash": "QmczcPVB8ppbVZJBZXGRVdtSVbGxK6s5YN8iD2xhypoXUG",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmbXtWp3Gvwbm6hHW39s4mJ4oUXBGfXS9QMCMitbcf2azm",
     "fileID": 4
   },
   {
     "name": "FF308",
     "artist": "Mr. Carmack",
     "fileHash": "QmeXvgzS9t67bD2Uwqo7MUn6uDWaTSrv2dDnQLZREhsZGc",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmdYZ8qErq4nf3t2tT28gES7iDdGNCePcxyKE3r29wvte4",
     "fileID": 5
   },
   {
     "name": "Asdf",
     "artist": "Octbr",
     "fileHash": "QmWXcSBwg3nKAsdGzp2b3cJAgs8KsYoPCFoFD2wMqcfNFb",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmYVaLnSdVTDEvVPEPmHabyCVV81WP9BPG7BvrMnv8xB9m",
     "fileID": 6
   },
   {
     "name": "Solo",
     "artist": "Joseph L'etranger",
     "fileHash": "QmUSEvioeAT975qmfjTczixGc2GXwrTUeSGAi7o4ukTyHF",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUBTAg3oVd68Yrrn887UtVwkbqbrbWCeuMVNTPT4zZQ1e",
     "fileID": 7
   },
   {
     "name": "On Sight",
     "artist": "88GLAM",
     "fileHash": "QmbRnx3XYFe6z2a2dYt9k4R2VTZbLTj1T3BybLq7miD9wf",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmXxGceMcNgYD5Q2WPoQTottahak3Gfvone2okLQdsabzc",
     "fileID": 8
   },
   {
     "name": "Panther",
     "artist": "Made in Heights",
     "fileHash": "QmUpNqpt2J1q9rpigcyMFoP3x14QXf8SZ7J5r2XGwpLMto",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmYqXXLiJDKneKn7pW25yxvYLQbNqyQFyr6iF2gJXqZbDc",
     "fileID": 9
   },
   {
     "name": " 10.9",
     "artist": "Sharad",
     "fileHash": "QmTYS5sgTbiwppnZZPT8G8k8J22GaJp48xJZuTUDq8QupK",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmV8VLuvq7hS1w4NWZ7jqixHAYv81i7Nx9pUgSpfDKKHew",
     "fileID": 10
   },
   {
     "name": "Swimming Pools",
     "artist": "Pierre Bourne",
     "fileHash": "QmTtdGJg5dToC2tj4oc8BEXeHBuT5iTgkoxpCHzV3AA7tQ",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmeYJt6uTbyi1cNobDj6KMgh5YLqQUmUimg1q72JptLdNQ",
     "fileID": 11
   },
   {
     "name": "BARDIEL",
     "artist": "Joseph L'etranger",
     "fileHash": "QmYwyNpJf5vMXGnMknqpmF7XMz3aEDk62obmuzfai5g3Rj",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUBTAg3oVd68Yrrn887UtVwkbqbrbWCeuMVNTPT4zZQ1e",
     "fileID": 12
   },
   {
     "name": "Airglow Fires",
     "artist": "Lone",
     "fileHash": "QmehvBw3Jd3zjRKFYp5z9zaJmwUV855iKjWq9qMYGDgU8e",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUbbiasUrZrAQ7Q3fQMir8SpdgW7gH7kkGy1uYhQSp1Vv",
     "fileID": 13
   },
   {
     "name": "How He Do That",
     "artist": "QuincyGotRich!",
     "fileHash": "QmZi9qToocma6wQ6JFmLST4QNMBZAQ2BK1E4yXqA7RTCxn",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUbbiasUrZrAQ7Q3fQMir8SpdgW7gH7kkGy1uYhQSp1Vv",
     "fileID": 14
   },
   {
     "name": "Slop",
     "artist": "XXX",
     "fileHash": "QmSw2SG5mGEh9nrLw3Dc1kbwUS9vTDNHvGx5j3h8P5MP9V",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmVMeqv8aTh1W3Vnq7SCrxf9SxUY2Dfz5SVMuiFDkHdtpf",
     "fileID": 15
   },
   {
@@ -122,49 +122,49 @@ const audioJSON = [
     "name": "Codeine Kiss",
     "artist": "Eric Dingus",
     "fileHash": "QmeJfj4zaTWHSnQy3R39Jf9SgRw3iWFvW2bMj48137QmAf",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmbXtWp3Gvwbm6hHW39s4mJ4oUXBGfXS9QMCMitbcf2azm",
     "fileID": 17
   },
   {
     "name": "Do Right",
     "artist": "Les Sins",
     "fileHash": "QmTFzkanxoiyouusLzghCLWMaAdhaJgD4eyUxe2WgeDyp5",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmPBUAukJpj6bsEjeddb1W7KRkKqsPjUqUVejgYpzMKuPn",
     "fileID": 18
   },
   {
     "name": "To The Moon",
     "artist": "SmokePurpp",
     "fileHash": "QmeA4R3FSGWkz2WXnprbtNvKsgEXFzm3tmXmG22EfDBM8v",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmTZ4MTRVqk6WVGN6cee5fsHf26wSfc3mZUTdTQfb5VCZh",
     "fileID": 19
   },
   {
     "name": "PLAYBOY",
     "artist": "Joseph L'etranger",
     "fileHash": "QmbME2YQHX1wnzQUyWzujNREGsj88ASAWHX3cgkXhvR2XJ",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmUBTAg3oVd68Yrrn887UtVwkbqbrbWCeuMVNTPT4zZQ1e",
     "fileID": 20
   },
   {
     "name": "Chantingz",
     "artist": "AshTreJenkins",
     "fileHash": "QmZFF11hviH8fgVyinkLJ8SmHRiWGQdfDMTs3FfrS7gZUg",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmPRSKjRyZsEJ1UVZN9zFTLx3gEV91zxP5y53JJqio3MSw",
     "fileID": 21
   },
   {
     "name": "Unreleased",
     "artist": "Rustie",
     "fileHash": "Qme8McCYXyRUbweGTjvQTaW87Zfq3gKL4v3QfGpQJbjzDf",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmSSPY4U1iBzNQ1FwFNDhdy2yNNEyxHryrATSJhY6Duj3u",
     "fileID": 22
   },
   {
     "name": "Blast",
     "artist": "Clams Casino",
     "fileHash": "QmYsT1MS4cuWoQAbKfK6r3r3VHYKAF4cPeEn5JZ4xCVmZr",
-    "imageHash": "QmR3bHFfkMrPwWF15xP8ikmhT2mmBsRz7b3HVCSKj5hbZw",
+    "imageHash": "QmTAvYGgpuXpQHX28v2X2M8JYWf15Y2mqVw1nk9ER9FANZ",
     "fileID": 23
   },
   {
